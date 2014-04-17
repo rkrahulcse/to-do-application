1929771e8d1b5c87c9057b83c6cc90303833827b
rkrahulcse <rkrahulcse93@gmail.com>
2014-04-18 01:29:34 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>To Do Application</title>
+<link type="text/css" />
+<link href="style.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body style="background-image:url(back.jpg)">
+<div align="center">
+<h1 align="center" style="color:#903"> Desto Creatives Solution</h1>
+<h2 align="center" style="color:#903"> To-Do Application</h2>
+</div>
+<br /><br /><br />
+<form action="login1.php" method="post">
+			
+            <div style="margin-left:500px; color:#903; font-size:20px">
+			<table>
+			<tr>
+                <td>Email-id:</td>
+				<td><input type="text" name="email" required="required" /></td>
+			</tr>
+			<tr></tr>
+			<tr>
+                <td>Password:</td>
+				<td><input type="password" name="password" required="required" /></td>
+			</tr>
+			<tr></tr>
+			<tr>
+        		<td colspan=2><input type="submit" name="submit" id="submit" value="Login" style="margin-left:90px"/></td>
+			</tr>
+			</table>
+            </div>
+                <br /><br />
+                    			
+    </form>
+    <br /><br /><br /><br />
+
+<p align="center" style="color:#903; font-size:20px">DESIGNED AND DEVELOPED BY RAHUL KUMAR </p>
+</body>
+</html>
